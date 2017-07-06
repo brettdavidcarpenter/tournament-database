b8547e1f7c60af13436751835ffadf567331ce33
brettdavidcarpenter <brettdavidcarpenter@outlook.com>
2017-07-05 18:33:44 -0700

    revised

--- tournament.sql
+++ tournament.sql
@@ -7,13 +7,14 @@
 -- these lines here.
 
 --
+DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 --
 \c tournament
 --
 create table Players(id serial primary key, name text);
 --
-create table Matches(result real, id integer references players);
+create table Matches(winner integer, loser integer, match id integer primary key);
 --
 create view standings as
     select players.id, players.name, COALESCE(sum(matches.result),0) AS wins, count(matches.result)
